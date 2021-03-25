7416b85fa509c7a9fb502695348233c591b6bf98
sa <alvarengasergio@hotmail.co.uk>
2021-03-25 16:02:54 +0100

    modified the app.js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // DOM Events
-const playerScore =  document.querySelector(".sc__player-count p");
-const cpuScore =  document.querySelector(".sc__cpu-count p");
+const playerScore = document.querySelector(".sc__player-count p");
+const cpuScore = document.querySelector(".sc__cpu-count p");
 const buttonChoices = document.querySelectorAll(".play-options button");
 const btnReset = document.getElementById("reset-game");
 const messageDisplay = document.querySelector(".display-winner p");
@@ -15,32 +15,32 @@ initialize();
 //Do not show the reset button when game loads
 btnReset.style.display = "none";
 
-function initialize (){
+function initialize() {
 
-    
-    buttonChoices.forEach(button=>{
-        button.addEventListener("click", function(){
+
+    buttonChoices.forEach(button => {
+        button.addEventListener("click", function () {
             //Show reset button
             btnReset.style.display = "block";
 
             cpuImg.src = "/assets/rock.png";
             playerImg.src = "/assets/rock.png";
-            
+
             const choices = ["rock", "paper", "scissors"];
 
             //pick a random sign for the CPU
             const choice = Math.floor(Math.random() * 3);
             const cpuHand = choices[choice];
-            
+
             const playerHand = this.textContent;
-            
-            setTimeout(()=>{
-            
-            //Grab and Update the images
-            cpuImg.src = `/assets/${cpuHand}.png`;
-            playerImg.src = `/assets/${playerHand}.png`;
-            
-            determineWinner(playerHand, cpuHand);
+
+            setTimeout(() => {
+
+                //Grab and Update the images
+                cpuImg.src = `/assets/${cpuHand}.png`;
+                playerImg.src = `/assets/${playerHand}.png`;
+
+                determineWinner(playerHand, cpuHand);
 
             }, 2300);
 
@@ -51,38 +51,35 @@ function initialize (){
         });
     });
 
-    const determineWinner = (playerHand, cpuHand) =>{
+    const determineWinner = (playerHand, cpuHand) => {
 
-        console.log(`Player Hand: ${playerHand}`);
-        console.log(`CPU Hand: ${cpuHand}`);
-        
-        if(playerHand === cpuHand){
+        if (playerHand === cpuHand) {
             messageDisplay.textContent = "It's a tie";
             return;
-        }else if (playerHand === "rock"){
-            if(cpuHand === "paper"){
+        } else if (playerHand === "rock") {
+            if (cpuHand === "paper") {
                 announceWinner("CPU");
                 return;
-            }else{
+            } else {
                 announceWinner("Player");
                 return;
             }
         }
 
-        else if(playerHand === "paper"){
-            if(cpuHand === "scissors"){
+        else if (playerHand === "paper") {
+            if (cpuHand === "scissors") {
                 announceWinner("CPU");
                 return;
-            }else{
+            } else {
                 announceWinner("Player");
                 return;
             }
         }
-        else if(playerHand === "scissors"){
-            if(cpuHand === "rock"){
+        else if (playerHand === "scissors") {
+            if (cpuHand === "rock") {
                 announceWinner("CPU");
                 return;
-            }else{
+            } else {
                 announceWinner("Player");
                 return;
             }
@@ -90,39 +87,39 @@ function initialize (){
     };
 
     //Announce Winner and update score
-    const announceWinner = (player)=>{
+    const announceWinner = (player) => {
         messageDisplay.textContent = `${player} Wins`;
         player === "CPU" ? cpuScore.textContent = ++cpuScoreCounter : playerScore.textContent = ++playerScoreCounter;
-    }      
+    }
 
 
     // Reset the animation
-    const resetAnimation = ()=>{
+    const resetAnimation = () => {
         const images = document.querySelectorAll("img");
-        images.forEach(image =>{
-            image.addEventListener("animationend",function(){
+        images.forEach(image => {
+            image.addEventListener("animationend", function () {
                 cpuImg.style.animation = "";
                 playerImg.style.animation = "";
             });
         })
     }
 
-    
 
-    btnReset.addEventListener("click", function (){
+
+    btnReset.addEventListener("click", function () {
         reset();
         this.style.display = "none";
     })
 
     //reset everything
-    function reset(){
+    function reset() {
         playerScoreCounter = 0;
         cpuScoreCounter = 0;
         cpuScore.textContent = cpuScoreCounter;
         playerScore.textContent = playerScoreCounter;
         cpuImg.src = "/assets/rock.png";
         playerImg.src = "/assets/rock.png";
-        
+
     }
-    
+
 }
